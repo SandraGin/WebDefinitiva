a91737a2251021be4a5fe8f7e486268219d66313
root <a19jicasa@iesgrancapitan.org>
2023-03-05 12:24:20 +0000

    bueno bueno

--- login.php
+++ login.php
@@ -1,8 +1,17 @@
 <?php
+session_start();
 include ('conexion.php');
 $email = $_POST['email'];
-$contrasena = $_POST ['contrasena']
+$contrasena = $_POST ['contrasena'];
 
 $consulta_login = "select * from clientes where email = '$email' AND contrasena = '$contrasena'";
-debug ($consulta_login)
+$resultado = mysqli_query($conexion, $consulta_login);
+if (mysqli_num_rows($resultado) > 0) {
+	// Iniciar sesión y redirigir al usuario a la página de inicio
+	$_SESSION['email'] = $email;
+	header('Location: index.html');
+} else {
+	// Mostrar un mensaje de error si el usuario no existe
+	echo "Correo electrónico o contraseña incorrectos.";
+}
 ?>
