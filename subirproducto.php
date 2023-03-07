c7b60683cedda1196ddf8e4fdb7a52b746329f1d
SandraGin <a19jicasa@iesgrancapitan.org>
2023-03-07 16:38:30 +0100

    brorborbobrojwdr

--- subirproducto.php
+++ subirproducto.php
@@ -6,16 +6,38 @@
     $cantidad = $_POST['cantidad'];
     $imagen = $_FILES['imagen']['name'];
     $descripcion = $_POST['descripcion'];
-
-  
-	if(move_uploaded_file($imagen, 'productos/'.$nombreproducto)){
-			echo "Archivo guardado con exito";
-	}else{
-			echo "Archivo no se pudo guardar";
-		}
+    $target_dir = "productos/";
+    $target_file = $target_dir . basename($imagen_nombre);
+    
+    // Verificar si el archivo es una imagen
+    $check = getimagesize($imagen_tmp);
+    if($check === false) {
+        echo "El archivo no es una imagen.";
+        exit();
+    }
+    
+    // Verificar si el archivo ya existe
+    if(file_exists($target_file)) {
+        echo "El archivo ya existe.";
+        exit();
+    }
+    
+    // Verificar si el archivo es demasiado grande
+    if($_FILES['imagen']['size'] > 5000000) {
+        echo "El archivo es demasiado grande.";
+        exit();
+    }
+    
+    // Subir el archivo al servidor
+    if(move_uploaded_file($imagen_tmp, $target_file)) {
+        echo "El archivo se ha subido correctamente.";
+    } else {
+        echo "No se pudo subir el archivo.";
+    }
+    
     // Insertar los datos en la base de datos
     $sql = "INSERT INTO productos (nombreproducto, categoria, precio, cantidad, imagen, descripcion) 
-            VALUES ('$nombreproducto', '$categoria', '$precio', '$cantidad', '$imagen', '$descripcion')";
+            VALUES ('$nombreproducto', '$categoria', '$precio', '$cantidad', '$imagen_nombre', '$descripcion')";
     if (mysqli_query($conn, $sql)) {
         echo "El producto se ha añadido correctamente.";
         mysqli_close($conn);
@@ -26,5 +48,4 @@
         mysqli_close($conn);
         
     }
-     // Cerrar la conexión a la base de datos
-?>
+?> 
